ff189490793e5d9815555e3ce33055bc312d3f29
Sarthak Khattar <sarthakoct@gmail.com>
2018-10-21 19:12:01 +0530

    Add files via upload

---
+++ doraemon_scraper.py
@@ -0,0 +1,53 @@
+# This Scrapes Doraemon RAW Manga using BeautifulSoup
+
+from bs4 import BeautifulSoup
+import requests
+import os
+
+url = "https://doraemon.mangawiki.org/read-manga/index.php?manga=Doraemon-Manga-Raw&chapter=Doraemon_Vol_01_RAW&page="
+i = 1
+img_url = []
+err = []
+
+while True:
+	try:
+		r = requests.get(url + str(i))
+		i += 1
+		data = r.text
+		soup = BeautifulSoup(data, "lxml")
+
+		manga_found = False
+
+		for link in soup.find_all('img'):
+			image = link.get("src")
+			img_url = "http://doraemon.mangawiki.org/read-manga/" + image 
+
+			err = soup.find_all('div', attrs = {"class": "warn"})
+			if "There is no selected page!" in err:
+				print("That's all of it!")
+				exit()
+				
+			if 'mangas' in img_url:
+				manga_found = True
+				img_name = os.path.split(img_url)[1]
+	
+				#Replace the text in the quotes with the path of your choice in the next 3 lines
+
+				img_name = os.path.join('c:/Users/sarth/Documents/Mangas/ドラえもん', img_name) 
+				if not os.path.exists('c:/Users/sarth/Documents/Mangas/ドラえもん'):
+					os.makedirs('c:/Users/sarth/Documents/Mangas/ドラえもん')
+
+				r2 = requests.get(img_url)
+				
+				with open(img_name, 'wb') as f:
+					f.write(r2.content)
+					
+		if manga_found == False:
+			print ("That's all!")
+			break
+
+	except:
+ 		print("There You Go!")
+ 		break
+
+ 	
